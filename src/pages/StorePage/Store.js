b3692384b860a893a866837e774c597f7e0e5e68
ramonlnas <lopessramon0@gmail.com>
2022-11-26 16:29:07 -0300

    Commita para poder ir para a main

--- src/pages/StorePage/Store.js
+++ src/pages/StorePage/Store.js
@@ -1,7 +1,38 @@
+import styled from "styled-components"
+
+
 export default function Store() {
     return (
-        <Container>
-            Hello
-        </Container>
+        <Conteiner>
+            <Sidebar>
+              <TypeItens>a</TypeItens>
+              <TypeItens>a</TypeItens>
+              <TypeItens>a</TypeItens>
+
+            </Sidebar>
+        </Conteiner>
     )
 };
+
+const Conteiner = styled.div `
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  padding: 20px;
+  
+`
+
+const Sidebar = styled.div `
+width: 30vw;
+height: 100vh;
+display: flex;
+flex-direction: column;
+`
+const TypeItens = styled.div `
+  width: 150px;
+  height: 85px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #6d2928;
+`
