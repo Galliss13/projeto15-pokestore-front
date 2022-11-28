e1ec336bc223adc577b44ae3d29879f5a26b1434
Galliss13 <matheusgalindo1313@gmail.com>
2022-11-27 22:01:30 -0300

    feat: layout parcial do cart

--- src/pages/CartPage/Cart.js
+++ src/pages/CartPage/Cart.js
@@ -1,9 +1,25 @@
 import Container from "../../components/Container";
+import Logo from "../../components/Logo";
+import { Cartdiv, Caption, Product, Image, Name, Amount, Price } from "./style";
 
 export default function Cart() {
     return (
         <Container>
-            Cart
+            <Logo/>
+            <Cartdiv>
+                <Caption>
+                    <span>Imagem</span>
+                    <span>Nome</span>
+                    <span>Quantidade</span>
+                    <span>Preço</span>
+                </Caption>
+                <Product>
+                    <Image></Image>
+                    <Name></Name>
+                    <Amount></Amount>
+                    <Price></Price>
+                </Product>
+            </Cartdiv>
         </Container>
     )
 };
